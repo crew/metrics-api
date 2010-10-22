bc71aca437e1270293c6e9ea30089083bd12fb5e
Alex Lee <lee@ccs.neu.edu>
2010-10-22 11:08:12 -0400

    Add doctests for wire.py

--- crew/metrics/wire.py
+++ crew/metrics/wire.py
@@ -6,26 +6,65 @@ import api
 
 
 def dumps(obj):
+    """
+    >>> class X(object):
+    ...     def __str__(self):
+    ...         return 'X'
+    >>> dumps({1: 2, 3: 4})
+    '{"1":2,"3":4}'
+    >>> dumps(X())
+    Traceback (most recent call last):
+        ...
+    SerializationException: X
+    """
     try:
         return json.dumps(obj, separators=(',', ':'))
     except:
-        raise api.SerializationException()
+        raise api.SerializationException(obj)
+
 
 def loads(s):
+    """
+    >>> loads('{"one": 2, "three": 4}') == {'one': 2, 'three': 4}
+    True
+    >>> loads('{')
+    Traceback (most recent call last):
+        ...
+    DeserializationException: {
+    """
     try:
         return json.loads(s)
     except:
-        raise api.DeserializationException()
+        raise api.DeserializationException(s)
 
 
 class Chunk(object):
+    """
+    >>> c = Chunk()
+    >>> c.data = {}
+    >>> c.data
+    {}
+    """
 
     def __init__(self, data=None):
+        """
+        >>> c = Chunk()
+        """
         self.data = data
 
     def to_network(self):
+        """
+        >>> c = Chunk({"hello": "world"})
+        >>> c.to_network()
+        '{"hello":"world"}'
+        """
         return dumps(self.data)
 
     @classmethod
     def from_network(cls, s):
+        """
+        >>> c = Chunk.from_network('{"hello":"world"}')
+        >>> c.data == {"hello": "world"}
+        True
+        """
         return cls(loads(s))
