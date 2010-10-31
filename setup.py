0baefdb7503eebe0ffd95ae1c905170af8c22a4b
Alex Lee <lee@ccs.neu.edu>
2010-10-31 15:58:40 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+setup(
+    name = 'crew.metrics.api',
+    version = '0.1',
+    packages = ['crew', 'crew.metrics'],
+    author = 'Crew',
+    author_email = 'crew@ccs.neu.edu',
+    description = 'Crew Metrics API',
+    keywords = 'crew',
+)
