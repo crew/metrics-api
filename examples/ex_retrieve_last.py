0ec0007b05a9f12264bbbc7afae61f15cf6203d0
Alex Lee <lee@ccs.neu.edu>
2011-02-24 18:26:27 -0500

    Update example for windows.

--- examples/ex_retrieve_last.py
+++ examples/ex_retrieve_last.py
@@ -5,11 +5,14 @@ from crew.metrics.httpapi import HttpAPI
 
 def main():
     # See ex_create.py
-    api = HttpAPI(namespace='ns', apikey='apikey', hostname='localhost',
+#    api = HttpAPI(namespace='ns', apikey='apikey', hostname='localhost',
+#        port=2000, timeout=20)
+#    print api.retrieve_last(attributes={'type': 'A'}, limit=2)
+#    print api.retrieve_last(attributes={'type': 'A'}, limit=3)
+#    print api.retrieve_last(attributes={'type': ['A', 'B']}, limit=5)
+    api = HttpAPI(namespace='windows', apikey='apikey', hostname='129.10.112.144',
         port=2000, timeout=20)
-    print api.retrieve_last(attributes={'type': 'A'}, limit=2)
-    print api.retrieve_last(attributes={'type': 'A'}, limit=3)
-    print api.retrieve_last(attributes={'type': ['A', 'B']}, limit=5)
+    print api.retrieve_last(attributes={'hostname': 'bulbasaur'})
 
 
 if __name__ == '__main__':
